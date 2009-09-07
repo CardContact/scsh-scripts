ea6318e861cfbe998748417d981ec9161da9541a
Frank Thater <frank.thater@cardcontact.de>
2009-09-07 14:54:26 +0000

    Added first version of EAC2 CV certificate generator

---
+++ icao/EAC2CVCertificateGenerator.js
@@ -0,0 +1,389 @@
+/*
+ *  ---------
+ * |.##> <##.|  Open Smart Card Development Platform (www.openscdp.org)
+ * |#       #|  
+ * |#       #|  Copyright (c) 1999-2006 CardContact Software & System Consulting
+ * |'##> <##'|  Andreas Schwier, 32429 Minden, Germany (www.cardcontact.de)
+ *  --------- 
+ *
+ *  This file is part of OpenSCDP.
+ *
+ *  OpenSCDP is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License version 2 as
+ *  published by the Free Software Foundation.
+ *
+ *  OpenSCDP is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with OpenSCDP; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * @fileoverview
+ * EAC2CVCertificateGenerator - Simple CV certificate generator class
+ * based on "Advanced Security Mechanisms for Machine Readable Travel Documents", Version 2.0
+ *
+ *
+ * TODO: For now we only support ECC crypto
+ *
+ */
+
+/*
+ * Write a byte string object to file
+ *
+ * The filename is mapped to the location of the script
+ *
+ * name		Name of file
+ * content	ByteString content for file
+ *
+ */
+function writeFileToDisk(name, content) {
+
+	// Map filename
+	var filename = GPSystem.mapFilename(name, GPSystem.CWD);
+	print("Writing " + filename);
+
+	var file = new java.io.FileOutputStream(filename);
+	file.write(content);
+	file.close();
+}
+
+
+
+/*
+ * Read a byte string object from file
+ *
+ * The filename is mapped to the location of the script
+ *
+ * name		Name of file
+ *
+ */
+function readFileFromDisk(name) {
+
+	// Map filename
+	var filename = GPSystem.mapFilename(name, GPSystem.CWD);
+	print("Reading " + filename);
+
+	var file = new java.io.FileInputStream(filename);
+	
+	var content = new ByteBuffer();
+	var buffer = new ByteString("                                                                                                                                                                                                                                                                ", ASCII);
+	var len;
+	
+	while ((len = file.read(buffer)) >= 0) {
+		content.append(buffer.bytes(0, len));
+	}
+	
+	file.close();
+	return(content.toByteString());
+}
+
+
+
+function findTag(tlv, tagNumber) {
+	
+	for(var i = 0; (i < tlv.elements) && (tlv.get(i).tag != tagNumber); i++) {		
+	}
+	
+	if (i == tlv.elements) {
+		throw new GPError("CVCertificateGenerator", TAG_NOT_FOUND, TAG_NOT_FOUND, "Tag " + tagNumber + "not in structure.");
+
+	}
+	
+	return tlv.get(i)
+}
+
+/*
+ * Convert x/y coordinates to uncompressed format
+ *
+ * x/y - coordinates of EC point
+ * 
+ * return ByteString containing compressed format
+ *
+ */ 
+function encodeUncompressedECPoint(x,y) {
+    
+    bb = new ByteBuffer();
+    
+    // uncompressed encoding
+    bb.append(new ByteString("04", HEX));
+    bb.append(new ByteString(x, HEX));
+    bb.append(new ByteString(y, HEX));
+    
+    return bb.toByteString();
+}
+
+
+/*
+ * Decode x/y coordinates from uncompressed format
+ *
+ * Bytestring containing the compressed point
+ * 
+ * return X/Y-value
+ *
+ */ 
+function decodeUncompressedECPoint(compressedPoint) {
+    
+    // Determine the size of the coordinates ignoring the indicator byte '04'
+    var length = compressedPoint.length - 1;
+    
+    var sizeOfCoordinate = length / 2;
+    
+    var xValue = compressedPoint.bytes(1, sizeOfCoordinate);
+    var yValue = compressedPoint.bytes(1 + sizeOfCoordinate, sizeOfCoordinate);
+    
+    return { x:xValue, y:yValue };
+} 
+
+
+
+/*
+ * Define a generator object for CV certificates
+ */
+
+// Constructor
+function EAC2CVCertificateGenerator(crypto) {
+	this.crypto = crypto;
+}
+
+
+EAC2CVCertificateGenerator.prototype.reset = function() {
+}
+
+
+
+EAC2CVCertificateGenerator.prototype.setProfileIdentifier = function(profileID) {
+	this.profileIdentifier = profileID;
+}
+
+
+
+EAC2CVCertificateGenerator.prototype.setCAR = function(CAR) {
+	this.CAR = CAR;
+}
+
+
+EAC2CVCertificateGenerator.prototype.setCHR = function(CHR) {
+	this.CHR = CHR;
+}
+
+
+EAC2CVCertificateGenerator.prototype.setEffectiveDate = function(effectiveDate) {
+	this.effectiveDate = effectiveDate;
+}
+
+
+EAC2CVCertificateGenerator.prototype.setExpiryDate = function(expiryDate) {
+	this.expiryDate = expiryDate;
+}
+
+
+EAC2CVCertificateGenerator.prototype.setChatOID = function(oid) {
+	this.chatOID = oid;
+}
+
+
+EAC2CVCertificateGenerator.prototype.setChatAuthorizationLevel = function(authLevel) {
+	this.chatAuthorizationLevel = authLevel;
+}
+
+
+EAC2CVCertificateGenerator.prototype.setTAAlgorithmIdentifier = function(oid) {
+	this.taOID = oid;
+}
+
+
+EAC2CVCertificateGenerator.prototype.setExtensions = function(extensions) {
+	this.extensions = extensions;
+}
+
+
+EAC2CVCertificateGenerator.prototype.setIncludeDomainParameters = function(value) {
+	this.includeDomainParameters = value;
+}
+
+EAC2CVCertificateGenerator.prototype.getCAR = function() {
+    var t = new ASN1("Certification Authority Reference", 0x42, new ByteString(this.CAR, ASCII));
+    return t;
+}
+
+
+EAC2CVCertificateGenerator.prototype.getCHR = function() {
+		
+	var t = new ASN1("Certification Holder Reference", 0x5F20, new ByteString(this.CHR, ASCII));
+	return t;
+}
+
+
+function convertDate(date) {
+
+	var temp = new ByteString(date, ASCII);
+	var bb = new ByteBuffer();
+	var singleByte;
+	
+	for (i=0; i < temp.length; i++) {
+		bb.append(temp.byteAt(i) - 0x30);
+	}
+	
+	return bb.toByteString();
+}
+
+
+EAC2CVCertificateGenerator.prototype.getEffectiveDate = function() {
+    var t = new ASN1("Certificate Effective Date", 0x5F25, convertDate(this.effectiveDate));
+    return t;
+}
+
+
+EAC2CVCertificateGenerator.prototype.getExpiryDate = function() {
+    var t = new ASN1("Certificate Expiration Date", 0x5F24, convertDate(this.expiryDate));
+    return t;
+}
+
+
+EAC2CVCertificateGenerator.prototype.getCHAT = function() {
+    var t = new ASN1("Certificate Holder Authorization Template", 0x7F4C);
+    
+    var oid = new ASN1("Object Identifier", ASN1.OBJECT_IDENTIFIER, this.chatOID);
+    var authLevel = new ASN1("Authorization Level", 0x53, this.chatAuthorizationLevel);
+    
+    t.add(oid);
+    t.add(authLevel);
+    
+    return t;
+}
+
+
+/**
+ * Strips leading zeros of a ByteString
+ *
+ * @param {ByteString} value the ByteString value
+ * @return the stripped ByteString object, may be an empty ByteString
+ * @type ByteString
+ */
+EAC2CVCertificateGenerator.prototype.stripLeadingZeros = function(value) {
+	var i = 0;
+	for (; (i < value.length) && (value.byteAt(i) == 0); i++);
+	
+	return value.right(value.length - i);
+}
+
+
+EAC2CVCertificateGenerator.prototype.getPublicKey = function() {
+
+	var t = new ASN1("Public Key", 0x7F49);
+    	
+    // Create empty public key object- this is just used to extract the domain parameters
+    var key = new Key();
+    key.setType(Key.PUBLIC);
+	key.setComponent(Key.ECC_CURVE_OID, this.publicKey.getComponent(Key.ECC_CURVE_OID)); 
+    
+    t.add(new ASN1("Object Identifier", 0x06, this.taOID));
+    
+    if (this.includeDomainParameters == true) {
+    
+    	t.add(new ASN1("Prime Modulus", 0x81, key.getComponent(Key.ECC_P)));
+    	t.add(new ASN1("First coefficient a", 0x82, key.getComponent(Key.ECC_A)));
+    	t.add(new ASN1("Second coefficient b", 0x83, key.getComponent(Key.ECC_B)));
+    
+    	t.add(new ASN1("Base Point G", 0x84, encodeUncompressedECPoint(key.getComponent(Key.ECC_GX), key.getComponent(Key.ECC_GY))));
+    
+    	t.add(new ASN1("Order of the base point", 0x85, key.getComponent(Key.ECC_N)));
+    
+    	t.add(new ASN1("Public Point y", 0x86, encodeUncompressedECPoint(this.publicKey.getComponent(Key.ECC_QX), this.publicKey.getComponent(Key.ECC_QY))));
+    
+    	t.add(new ASN1("Cofactor f", 0x87, this.stripLeadingZeros(key.getComponent(Key.ECC_H))));
+    }
+        
+    return t;
+}
+
+
+EAC2CVCertificateGenerator.prototype.getProfileIdentifier = function() {
+
+	var bb = new ByteBuffer();
+	bb.append(this.profileIdentifier);
+	
+	var t = new ASN1("Certificate Profile Identifier", 0x5F29, bb.toByteString());
+	return t;
+}
+
+
+EAC2CVCertificateGenerator.prototype.getExtensions = function() {
+    var t = new ASN1("Certificate Extentions", 0x7F49);
+    return t;
+}
+
+
+EAC2CVCertificateGenerator.prototype.getCertificateBody = function() {
+	
+    var t = new ASN1("Certificate Body", 0x7F4E);
+    
+    t.add(this.getProfileIdentifier());
+    
+    t.add(this.getCAR());
+    
+    t.add(this.getPublicKey());
+    
+    t.add(this.getCHR());
+    
+    t.add(this.getCHAT());
+    
+    t.add(this.getEffectiveDate());
+    
+    t.add(this.getExpiryDate());
+    
+    if (this.extensions) {
+        t.add(this.getExtensions());
+	}
+	
+    return t;
+}
+
+
+EAC2CVCertificateGenerator.prototype.setPublicKey = function(publicKey) {
+	this.publicKey = publicKey;
+}
+
+
+/*
+EAC2CVCertificateGenerator.prototype.generateCVCertificate = function(request) {
+	
+	// requestInfos = this.verifyRequest(request);
+	
+	var certificate = new ASN1("CV Certificate", 0x7F21);
+	
+	var body = this.getCertificateBody(requestInfos.publicKey);
+    
+	certificate.add(body);
+	
+	var signature = this.crypto.sign(this.privateKey, Crypto.ECDSA_SHA256, body.getBytes());
+	var signatureValue = new ASN1("Signature", 0x5F37, signature);
+	
+    certificate.add(signatureValue);
+	
+    print(certificate);
+	return certificate.getBytes();
+}
+*/
+
+
+EAC2CVCertificateGenerator.prototype.generateCVCertificate = function(signingKey) {
+	
+	var certificate = new ASN1("CV Certificate", 0x7F21);
+	
+	var body = this.getCertificateBody();
+	
+	var signature = this.crypto.sign(signingKey, Crypto.ECDSA_SHA256, body.getBytes());
+	var signatureValue = new ASN1("Signature", 0x5F37, signature);
+	
+    certificate.add(body);
+    	
+	certificate.add(signatureValue);
+	
+	assert(crypto.verify(this.publicKey, Crypto.ECDSA_SHA256, body.getBytes(), signature));
+		
+    return certificate.getBytes();
+}
---
+++ icao/EAC2CVCertificateGeneratorTest.js
@@ -0,0 +1,188 @@
+//
+//  ---------
+// |.##> <##.|  CardContact Software & System Consulting
+// |#       #|  32429 Minden, Germany (www.cardcontact.de)
+// |#       #|  Copyright (c) 1999-2005. All rights reserved
+// |'##> <##'|  See file COPYING for details on licensing
+//  --------- 
+//
+// EAC2CVCertificateGenerator Tests and API Documentation
+//
+
+/// <?xml version="1.0" encoding="ISO-8859-1"?>
+/// <?xml-stylesheet type="text/xsl" href="docclass.xsl" ?>
+/// <class xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+///     xsi:noNamespaceSchemaLocation="api_doc.xsd" name="X509">
+///     <description><p>Class implementing support for CV certificates specified by TR-03110 Version 2.0</p>
+///     <constant name="XOR" type="Number">Constant used in crc() method</constant>
+///     <field name="length" type="Number">Number of bytes in the <type>ByteString</type></field>
+///     <method name="">
+///         <signature></signature>
+///         <description></description>
+///         <argument name="" type=""></argument>
+///         <return type=""></return>
+///         <example>
+///         </example>
+///         <exception name="GPError" value="GPError.ARGUMENTS_MISSING">Too few arguments in call</exception>
+///         <exception name="GPError" value="GPError.INVALID_ARGUMENTS">Too many arguments in call</exception>
+///         <exception name="GPError" value="GPError.INVALID_TYPE">Type of argument is invalid for call</exception>
+///     </method>
+
+load("EAC2CVCertificateGenerator.js");
+
+///     <constructor>
+///         <signature>EAC2CVCertificateGenerator(Crypto crypto)</signature>
+///         <description>Create the certificate generator using the specified crypto object.</description>
+///         <argument name="crypto" type="Crypto">The crypto engined to be used</argument>
+///         <example>
+
+var crypto = new Crypto();
+
+generator = new EAC2CVCertificateGenerator(crypto);
+
+///         </example>
+///         <exception name="GPError" value="GPError.ARGUMENTS_MISSING">Too few arguments in call</exception>
+///         <exception name="GPError" value="GPError.INVALID_ARGUMENTS">Too many arguments in call</exception>
+///         <exception name="GPError" value="GPError.INVALID_TYPE">Type of argument is invalid for call</exception>
+
+///     <method name="setCAR">
+///         <signature>void setCAR(String car)</signature>
+///         <description><p>Set the certification authority reference for the generated certificate</p></description>
+///         <example>
+
+var CAR = "decvca00000";
+
+generator.setCAR(CAR);
+
+///         </example>
+///     </method>
+
+///     <method name="setCHR">
+///         <signature>void setCHR(String chr)</signature>
+///         <description><p>Set the certificate holder reference for the generated certificate</p></description>
+///         <example>
+
+var CHR = "decvca00000";
+
+generator.setCHR(CHR);
+
+///         </example>
+///     </method>
+
+///     <method name="setEffectiveDate">
+///         <signature>void setEffectiveDate(String effectiveDate)</signature>
+///         <description><p>Set the effective date for the generated certificate</p></description>
+///         <example>
+
+var notBefore = "090210";
+
+d = new Date();
+
+d.setUTCFullYear(2009, 02, 10);
+print(d.toString());
+
+generator.setEffectiveDate(notBefore);
+
+///         </example>
+///     </method>
+
+///     <method name="setExpiryDate">
+///         <signature>void setExpiryDate(String expiryDate)</signature>
+///         <description><p>Set the expiry date for the generated certificate</p></description>
+///         <example>
+
+var notAfter = "110225";
+generator.setExpiryDate(notAfter);
+
+///         </example>
+///     </method>
+
+///     <method name="setChatOID">
+///         <signature>void setChatOID(ByteString oid)</signature>
+///         <description><p>Set the object identifier of the authorization template for the generated certificate</p></description>
+///         <example>
+
+var chatOID = "0.4.0.127.0.7.3.1.2.1"; // inspection system
+
+generator.setChatOID(new ByteString(chatOID, OID));
+
+///         </example>
+///     </method>
+
+///     <method name="setChatAuthorizationLevel">
+///         <signature>void setChatAuthorizationLevel(ByteString authLevel)</signature>
+///         <description><p>Set the authorization level of the authorization template for the generated certificate</p></description>
+///         <example>
+
+var chatAuth = "E3"; // CVCA, read access to eID, DG3, DG4
+
+generator.setChatAuthorizationLevel(new ByteString(chatAuth, HEX));
+
+///         </example>
+///     </method>
+
+///     <method name="setPublicKey">
+///         <signature>void setPublicKey(Key publicKey)</signature>
+///         <description><p>Set the public key for the generated certificate</p></description>
+///         <example>
+
+// Create empty public key object
+var pubKey = new Key("kp_cvca_ec_public.xml");
+
+generator.setPublicKey(pubKey);
+
+///         </example>
+///     </method>
+
+///     <method name="setProfileIdentifier">
+///         <signature>void setProfileIdentifier(UnsignedInteger profileID)</signature>
+///         <description><p>Set the profile identifier for the generated certificate</p></description>
+///         <example>
+
+var profileIdentifier = 0x00;
+
+generator.setProfileIdentifier(profileIdentifier);
+
+///         </example>
+///     </method>
+
+///     <method name="setTAAlgorithmIdentifier">
+///         <signature>void setProfileIdentifier(UnsignedInteger profileID)</signature>
+///         <description><p>Set the algorithm identifier for TA as specified in appendix A.6.4</p></description>
+///         <example>
+
+var taAlgorithmIdentifier = "0.4.0.127.0.7.2.2.2.2.3"; // ECDSA - SHA 256
+
+generator.setTAAlgorithmIdentifier(new ByteString(taAlgorithmIdentifier, OID));
+
+///         </example>
+///     </method>
+
+///     <method name="setIncludeDomainParameters">
+///         <signature>void setProfileIdentifier(boolean indicator)</signature>
+///         <description><p>Specify whether domain parameters should be included in the generated certificate.</p></description>
+///         <example>
+
+generator.setIncludeDomainParameters(true);
+
+///         </example>
+///     </method>
+
+///     <method name="generateCVCertificate">
+///         <signature>ByteString generateCVCertificate(Key privateKey)</signature>
+///         <description><p>Create a CV certificate.</p></description>
+///         <example>
+
+// Create empty private key object
+var priKey = new Key("kp_cvca_ec_private.xml");
+
+var certificate = generator.generateCVCertificate(priKey);
+
+///         </example>
+///     </method>
+
+writeFileToDisk("cvca-certificate.crt", certificate);
+
+outline = new OutlineNode("CV-Certificate");
+outline.insert(new ASN1(certificate));
+outline.show();
