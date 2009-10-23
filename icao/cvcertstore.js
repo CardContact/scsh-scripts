66d892d0d98c35af0fbacaf36bf623ab395f5549
Andreas Schwier <andreas.schwier@cardcontact.de>
2009-10-23 14:36:05 +0000

    Added Terminal Authentication

---
+++ icao/cvcertstore.js
@@ -0,0 +1,306 @@
+/**
+ *  ---------
+ * |.##> <##.|  Open Smart Card Development Platform (www.openscdp.org)
+ * |#       #|  
+ * |#       #|  Copyright (c) 1999-2009 CardContact Software & System Consulting
+ * |'##> <##'|  Andreas Schwier, 32429 Minden, Germany (www.cardcontact.de)
+ *  --------- 
+ *
+ *  This file is part of OpenSCDP.
+ *
+ *  OpenSCDP is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License version 2 as
+ *  published by the Free Software Foundation.
+ *
+ *  OpenSCDP is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with OpenSCDP; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ * 
+ * @fileoverview Support for card verifiable certificates according to EAC 2.0
+ */
+ 
+
+
+
+load("pkcs8.js");
+
+
+
+/**
+ * Create a object to access a certificate store
+ *
+ * @param {String} path the root of the certificate store
+ */
+function CVCertificateStore(path) {
+	this.path = path;
+}
+
+
+
+/**
+ * Loads a binary file from disk
+ *
+ * @param {String} filename the fully qualified file name
+ * @return the binary content
+ * @type ByteString
+ */
+CVCertificateStore.loadBinaryFile = function(filename) {
+	// Open stream
+	var f = new java.io.FileInputStream(filename);
+	
+	// Determine file size
+	var flen = f.available();
+
+	// Allocate native byte array
+	var bs = java.lang.reflect.Array.newInstance(java.lang.Byte.TYPE, flen);
+	
+	// Read into byte array
+	var len = f.read(bs);
+
+	// Allocate JavaScript ByteBuffer from native/wrapped byte array
+	var bb = new ByteBuffer(bs);
+	
+	// Convert to JavaScript ByteString
+	var data = bb.toByteString();
+
+	return data;
+}
+
+
+
+/**
+ */
+CVCertificateStore.prototype.getTerminalCertificateFor = function(cvcaref) {
+	var fn = this.path + "/" + cvcaref.getHolder() + "/terminal/current.cvcert";
+	
+	var bin = CVCertificateStore.loadBinaryFile(fn);
+	
+	var cvc = new CVC(bin);
+	
+	return cvc;
+}
+
+
+
+CVCertificateStore.prototype.getDVCACertificateFor = function(cvcaref, dvcaref) {
+	var fn = this.path + "/" + cvcaref.getHolder() + "/" + dvcaref.getHolder() + "/" + dvcaref.toString() + ".cvcert";
+	
+	var bin = CVCertificateStore.loadBinaryFile(fn);
+	
+	var cvc = new CVC(bin);
+	
+	return cvc;
+}
+
+
+
+CVCertificateStore.prototype.getCVCACertificateFor = function(cvcaref) {
+	var fn = this.path + "/" + cvcaref.getHolder() + "/" + cvcaref.toString() + ".cvcert";
+	
+	var bin = CVCertificateStore.loadBinaryFile(fn);
+	
+	var cvc = new CVC(bin);
+	
+	return cvc;
+}
+
+
+
+CVCertificateStore.prototype.getCertificateChainFor = function(cvcaref) {
+	var chain = new Array();
+	
+	var termcert = this.getTerminalCertificateFor(cvcaref);
+	chain.push(termcert);
+	
+	var dvcaref = termcert.getCAR();
+	
+	var dvcacert = this.getDVCACertificateFor(cvcaref, dvcaref);
+	chain.push(dvcacert);
+	
+	var ref = dvcacert.getCAR();
+	
+	while (ref.toString() != cvcaref.toString()) {
+		var cvcacert = this.getCVCACertificateFor(cvcaref);
+		chain.push(cvcacert);
+		
+		var ref = cvcacert.getCAR();
+	}
+
+	return(chain);
+}
+
+
+
+CVCertificateStore.prototype.getTerminalKeyFor = function(cvcaref) {
+	var fn = this.path + "/" + cvcaref.getHolder() + "/terminal/current.pkcs8";
+	
+	var bin = CVCertificateStore.loadBinaryFile(fn);
+
+	return PKCS8.decodeKeyFromPKCS8Format(bin);
+}
+
+
+
+CVCertificateStore.testPath = GPSystem.mapFilename("cvc", GPSystem.CWD);
+
+
+CVCertificateStore.test = function() {
+	var ss = new CVCertificateStore(CVCertificateStore.testPath);
+	
+	var pkr = new PublicKeyReference("TPCVCAAT00001");
+	
+	var cvc = ss.getTerminalCertificateFor(pkr);
+	
+	print(cvc);
+	
+	print(cvc.getCAR());
+	
+	var cvcchain = ss.getCertificateChainFor(pkr);
+	
+	for (var i = cvcchain.length - 1; i >= 0; i--) {
+		print(cvcchain[i]);
+	}
+}
+
+
+
+function CVC() {
+	if (arguments.length > 0) {
+		this.bin = arguments[0];
+		if (this.bin.bytes(0, 2).toString(HEX) != "7F21") {
+			throw new GPError("CVC", GPError.INVALID_DATA, 0, "Data does not seem to be CV certificate");
+		}
+		this.asn = new ASN1(this.bin);
+	}
+}
+
+
+
+CVC.prototype.getCAR = function() {
+	var cardo = this.asn.get(0).find(CVC.TAG_CAR);
+	
+	if (!cardo) {
+		throw new GPError("CVC", GPError.OBJECT_NOT_FOUND, 0, "Certificate does not contain a CAR");
+	}
+	
+	return new PublicKeyReference(cardo.value);
+}
+
+
+
+CVC.prototype.getCHR = function() {
+	var chrdo = this.asn.get(0).find(CVC.TAG_CHR);
+	
+	if (!chrdo) {
+		throw new GPError("CVC", GPError.OBJECT_NOT_FOUND, 0, "Certificate does not contain a CHR");
+	}
+	
+	return new PublicKeyReference(chrdo.value);
+}
+
+
+
+CVC.prototype.getBytes = function() {
+	return this.bin;
+}
+
+
+
+CVC.prototype.toString = function() {
+	var str = "CVC CAR=" + this.getCAR().toString() + " CHR=" + this.getCHR().toString();
+	return str;
+}
+
+
+
+CVC.TAG_CPI = 0x5F29;
+CVC.TAG_CAR = 0x42;
+CVC.TAG_CHR = 0x5F20;
+
+
+
+/**
+ * Create a public key reference (CAR/CHR) from binary representation or individual fields
+ */
+function PublicKeyReference() {
+	if (arguments.length > 0) {
+		if (arguments.length == 1) {
+			if (typeof(arguments[0]) == "string") {
+				this.bin = new ByteString(arguments[0], ASCII);
+			} else {
+				this.bin = arguments[0];
+			}
+		} else {
+			var cc = arguments[0];
+			var mn = arguments[1];
+			var sq = arguments[2];
+			this.bin = new ByteString(cc + mn + sq, ASCII);
+		}
+	}
+}
+
+
+
+PublicKeyReference.prototype.getCountryCode = function() {
+	return this.bin.bytes(0, 2).toString(ASCII);
+}
+
+
+
+PublicKeyReference.prototype.getMnemonic = function() {
+	return this.bin.bytes(2, this.bin.length - 7).toString(ASCII);
+}
+
+
+
+PublicKeyReference.prototype.getSequenceNo = function() {
+	return this.bin.bytes(this.bin.length - 5, 5).toString(ASCII);
+}
+
+
+
+PublicKeyReference.prototype.getHolder = function() {
+	return this.getCountryCode() + this.getMnemonic();
+}
+
+
+
+PublicKeyReference.prototype.getBytes = function() {
+	return this.bin;
+}
+
+
+
+PublicKeyReference.prototype.toString = function() {
+	return this.bin.toString(ASCII);
+}
+
+
+
+PublicKeyReference.test = function() {
+	var p = new PublicKeyReference(new ByteString("UTABCDF0000", ASCII));
+	assert(p.getCountryCode() == "UT");
+	assert(p.getMnemonic() == "ABCD");
+	assert(p.getSequenceNo() == "F0000");
+	assert(p.getHolder() == "UTABCD");
+	
+	var p = new PublicKeyReference("UT", "ABCD", "F0000");
+	assert(p.getCountryCode() == "UT");
+	assert(p.getMnemonic() == "ABCD");
+	assert(p.getSequenceNo() == "F0000");
+	assert(p.getHolder() == "UTABCD");
+	
+	var p = new PublicKeyReference("UTABCDF0000");
+	assert(p.getCountryCode() == "UT");
+	assert(p.getMnemonic() == "ABCD");
+	assert(p.getSequenceNo() == "F0000");
+	assert(p.getHolder() == "UTABCD");
+	
+	assert(p.getBytes().toString(ASCII) == "UTABCDF0000");
+}
+
--- icao/eac20.js
+++ icao/eac20.js
@@ -23,94 +23,288 @@
  */
  
 load("tools/eccutils.js");
-load("eac20sim.js");
+// load("eac20sim.js");
+
+load("cvcertstore.js");
 
 
 
+function EAC20(crypto, card) {
+	this.crypto = crypto;
+	this.card = card;
+	this.sm = null;
+	
+	// ToDo: Read from EF_CardInfo
+	this.oidPACE = PACE.id_PACE_ECDH_GM_AES_CBC_CMAC_128;
+	this.oidTerminalAuthentication = EAC20.id_TA_ECDSA_SHA_256;
 
-var card = new EAC20Sim();
-// var card = new Card(_scsh3.reader);
+	var dp = new Key();
+	dp.setComponent(Key.ECC_CURVE_OID, ECCUtils.brainpoolP256r1);
+	this.domainParameterPACE = dp;
+	this.domainParameterCA = dp;
+}
 
-var dp = new Key();
-dp.setComponent(Key.ECC_CURVE_OID, ECCUtils.brainpoolP256r1);
 
-var pace = new PACE(PACE.id_PACE_ECDH_GM_AES_CBC_CMAC_128, dp);
-pace.setPassword(new ByteString("000001", ASCII));
+
+EAC20.prototype.doPACE = function(id, pwd, chat) {
+
+	// ToDo: Read from EF_CardInfo
+	
+	var pace = new PACE(this.oidPACE, this.domainParameterPACE);
+	pace.setPassword(pwd);
+
+	// Manage SE
+	var crt = new ByteBuffer();
+	crt.append((new ASN1(0x80, new ByteString(PACE.id_PACE_ECDH_GM_AES_CBC_CMAC_128, OID))).getBytes());
+	crt.append(new ByteString("8301", HEX));
+	crt.append(id);
+	crt.append(chat.getBytes());
+
+	card.sendSecMsgApdu(Card.CPRO|Card.CENC|Card.RPRO, 0x00, 0x22, 0xC1, 0xA4, crt.toByteString(), [0x9000, 0x63C2, 0x63C1, 0x63C0, 0x6283 ]);
+
+
+	// General Authenticate
+	var dado = new ASN1(0x7C);
+
+	dadobin = card.sendSecMsgApdu(Card.CPRO|Card.CENC|Card.RPRO|Card.RENC, 0x10, 0x86, 0x00, 0x00, dado.getBytes(), 0, [0x9000]);
+
+	var dado = new ASN1(dadobin);
+	assert(dado.tag == 0x7C);
+	assert(dado.elements == 1);
+	var encryptedNonceDO = dado.get(0);
+	assert(encryptedNonceDO.tag == 0x80);
+	var encryptedNonce = encryptedNonceDO.value;
+
+	GPSystem.trace("Encrypted nonce: " + encryptedNonce);
+
+	pace.decryptNonce(encryptedNonce);
+
+	var mappingData = pace.getMappingData();
+
+	var dado = new ASN1(0x7C, new ASN1(0x81, mappingData));
+
+	dadobin = card.sendSecMsgApdu(Card.CPRO|Card.CENC|Card.RPRO|Card.RENC, 0x10, 0x86, 0x00, 0x00, dado.getBytes(), 0, [0x9000]);
+
+	var dado = new ASN1(dadobin);
+	assert(dado.tag == 0x7C);
+	assert(dado.elements == 1);
+	var mappingDataDO = dado.get(0);
+	assert(mappingDataDO.tag == 0x82);
+
+	pace.performMapping(mappingDataDO.value);
+
+	var ephemeralPublicKeyIfd = pace.getEphemeralPublicKey();
+
+	var dado = new ASN1(0x7C, new ASN1(0x83, ephemeralPublicKeyIfd));
+
+	dadobin = card.sendSecMsgApdu(Card.CPRO|Card.CENC|Card.RPRO|Card.RENC, 0x10, 0x86, 0x00, 0x00, dado.getBytes(), 0, [0x9000]);
+
+	var dado = new ASN1(dadobin);
+	assert(dado.tag == 0x7C);
+	assert(dado.elements == 1);
+	var ephemeralPublicKeyICC = dado.get(0);
+	assert(ephemeralPublicKeyICC.tag == 0x84);
+
+	this.idPicc = ephemeralPublicKeyICC.value.bytes(1, (ephemeralPublicKeyICC.value.length - 1) >> 1);
+	GPSystem.trace("ID_PICC : " + this.idPicc);
+	
+	pace.performKeyAgreement(ephemeralPublicKeyICC.value);
+
+
+	var authToken = pace.calculateAuthenticationToken();
+
+	var dado = new ASN1(0x7C, new ASN1(0x85, authToken));
+
+	dadobin = card.sendSecMsgApdu(Card.CPRO|Card.CENC|Card.RPRO|Card.RENC, 0x00, 0x86, 0x00, 0x00, dado.getBytes(), 0, [0x9000, 0x63C2, 0x63C1, 0x63C0, 0x6283 ]);
+
+	var dado = new ASN1(dadobin);
+	GPSystem.trace(dado);
+	assert(dado.tag == 0x7C);
+	assert(dado.elements >= 1);
+	assert(dado.elements <= 3);
+	var authTokenDO = dado.get(0);
+	assert(authTokenDO.tag == 0x86);
+
+	if (dado.elements > 1) {
+		var cardo = dado.get(1);
+		assert(cardo.tag == 0x87);
+		this.lastCAR = new PublicKeyReference(cardo.value);
+	}
+	
+	if (dado.elements > 2) {
+		var cardo = dado.get(2);
+		assert(cardo.tag == 0x88);
+		this.previousCAR = new PublicKeyReference(cardo.value);
+	}
+
+	var sm = null;
+	
+	if (pace.verifyAuthenticationToken(authTokenDO.value)) {
+		GPSystem.trace("Authentication token valid");
+
+		sm = new IsoSecureChannel(crypto, IsoSecureChannel.SSC_SYNC_ENC_POLICY);
+		sm.setEncKey(pace.kenc);
+		sm.setMacKey(pace.kmac);
+		sm.setMACSendSequenceCounter(new ByteString("00000000000000000000000000000000", HEX));
+		
+	}
+
+	this.sm = sm;
+	return sm;
+}
+
+
+
+EAC20.prototype.getTrustAnchorCAR = function(previous) {
+	if (previous) {
+		return this.previousCAR;
+	} else {
+		return this.lastCAR;
+	}
+}
+
+
+
+EAC20.prototype.verifyCertificateChain = function(cvcchain) {
+	for (var i = cvcchain.length - 1; i >= 0; i--) {
+		var cvc = cvcchain[i];
+		
+		var car = cvc.getCAR().getBytes();
+		
+		var pukrefdo = new ASN1(0x83, car);
+		var pukref = pukrefdo.getBytes();
+		
+		this.card.sendSecMsgApdu(Card.CPRO|Card.CENC|Card.RPRO, 0x00, 0x22, 0x81, 0xB6, pukref, [0x9000]);
+		
+		// Extract value of 7F21
+		var tl = new TLVList(cvc.getBytes(), TLV.EMV);
+		var t = tl.index(0);
+		var v = t.getValue();
+		
+		this.card.sendSecMsgApdu(Card.CPRO|Card.CENC|Card.RPRO, 0x00, 0x2A, 0x00, 0xBE, v, [0x9000]);
+	}
+	
+	this.terminalCHR = cvc.getCHR();
+}
+
+
+
+EAC20.prototype.generateEphemeralCAKeyPair = function() {
+	this.prkCA = new Key(this.domainParameterCA);
+	this.prkCA.setType(Key.PRIVATE);
+	
+	this.pukCA = new Key(this.domainParameterCA);
+	this.pukCA.setType(Key.PUBLIC);
+	
+	this.crypto.generateKeyPair(Crypto.EC, this.pukCA, this.prkCA);
+	
+	return (this.pukCA.getComponent(Key.ECC_QX));
+}
+
+
+
+EAC20.prototype.performTerminalAuthentication = function(termkey, auxdata) {
+
+	var idIFD = this.generateEphemeralCAKeyPair();
+
+	var bb = new ByteBuffer();
+	bb.append(new ASN1(0x80, new ByteString(this.oidTerminalAuthentication, OID)).getBytes());
+	bb.append(new ASN1(0x83, this.terminalCHR.getBytes()).getBytes());
+//	bb.append(auxdata);
+	bb.append(new ASN1(0x91, idIFD).getBytes());
+	
+	var msedata = bb.toByteString();
+	print(msedata);
+	this.card.sendSecMsgApdu(Card.CPRO|Card.CENC|Card.RPRO, 0x00, 0x22, 0x81, 0xA4, msedata, [0x9000]);
+	
+	var challenge = this.card.sendSecMsgApdu(Card.CPRO|Card.CENC|Card.RPRO|Card.RENC, 0x00, 0x84, 0x00, 0x00, 8, [0x9000]);
+	
+	var bb = new ByteBuffer();
+	bb.append(this.idPicc);
+	bb.append(challenge);
+	bb.append(idIFD);
+	
+	var signatureInput = bb.toByteString();
+	print("Signature Input:");
+	print(signatureInput);
+	var signature = this.crypto.sign(termkey, Crypto.ECDSA_SHA256, signatureInput);
+	print("Signature (Encoded):");
+	print(signature);
+
+	signature = ECCUtils.unwrapSignature(signature);
+	print("Signature (Encoded):");
+	print(signature);
+
+	this.card.sendSecMsgApdu(Card.CPRO|Card.CENC|Card.RPRO, 0x00, 0x82, 0x00, 0x00, signature, [0x9000]);
+	
+}
+
+
+
+EAC20.ID_MRZ = 1;
+EAC20.ID_CAN = 2;
+EAC20.ID_PIN = 3;
+EAC20.ID_PUK = 4;
+
+EAC20.bsi_de = "0.4.0.127.0.7";
+EAC20.id_TA = EAC20.bsi_de + ".2.2.2";
+EAC20.id_TA_RSA = EAC20.id_TA + ".1";
+
+EAC20.id_TA_ECDSA = EAC20.id_TA + ".2";
+EAC20.id_TA_ECDSA_SHA_1 = EAC20.id_TA_ECDSA + ".1";
+EAC20.id_TA_ECDSA_SHA_224 = EAC20.id_TA_ECDSA + ".2";
+EAC20.id_TA_ECDSA_SHA_256 = EAC20.id_TA_ECDSA + ".3";
+
+
+
+var crypto = new Crypto();
+
+var certstorepath = GPSystem.mapFilename("cvc", GPSystem.CWD);
+
+var certstore = new CVCertificateStore(certstorepath);
+
+
+// var card = new EAC20Sim();
+var card = new Card(_scsh3.reader);
 
 card.reset(Card.RESET_COLD);
 
+var mf = new CardFile(card, ":3F00");
 
-// Manage SE
-var crt = new ByteBuffer();
-crt.append((new ASN1(0x80, new ByteString(PACE.id_PACE_ECDH_GM_AES_CBC_CMAC_128, OID))).getBytes());
-crt.append(new ByteString("830102", HEX));
+var chat = new ASN1(0x7F4C, 
+						new ASN1(ASN1.OBJECT_IDENTIFIER, new ByteString(PACE.id_IS, OID)),
+						new ASN1(0x53, new ByteString("23", HEX))
+					);
 
-card.sendApdu(0x00, 0x22, 0xC1, 0xA4, crt.toByteString());
-assert(card.SW == 0x9000);
+var eac = new EAC20(crypto, card);
 
+var pwd = new ByteString("000001", ASCII);
+var sm = eac.doPACE(EAC20.ID_CAN, pwd, chat);
 
-// General Authenticate
-var dado = new ASN1(0x7C);
+card.setCredential(sm);
 
-dadobin = card.sendApdu(0x10, 0x86, 0x00, 0x00, dado.getBytes(), 0);
-assert(card.SW == 0x9000);
+var car = eac.getTrustAnchorCAR(false);
 
-var dado = new ASN1(dadobin);
-assert(dado.tag == 0x7C);
-assert(dado.elements == 1);
-var encryptedNonceDO = dado.get(0);
-assert(encryptedNonceDO.tag == 0x80);
-var encryptedNonce = encryptedNonceDO.value;
+var cvcchain = certstore.getCertificateChainFor(car);
 
-print("Encrypted nonce: " + encryptedNonce);
+eac.verifyCertificateChain(cvcchain);
 
-pace.decryptNonce(encryptedNonce);
+// Get key for terminal certificate
+var termkey = certstore.getTerminalKeyFor(car);
 
-var mappingData = pace.getMappingData();
+var ad = new ASN1(0x67);
 
-var dado = new ASN1(0x7C, new ASN1(0x81, mappingData));
-
-dadobin = card.sendApdu(0x10, 0x86, 0x00, 0x00, dado.getBytes(), 0);
-
-var dado = new ASN1(dadobin);
-assert(dado.tag == 0x7C);
-assert(dado.elements == 1);
-var mappingDataDO = dado.get(0);
-assert(mappingDataDO.tag == 0x82);
-
-pace.performMapping(mappingDataDO.value);
-
-var ephemeralPublicKeyIfd = pace.getEphemeralPublicKey();
-
-var dado = new ASN1(0x7C, new ASN1(0x83, ephemeralPublicKeyIfd));
-
-dadobin = card.sendApdu(0x10, 0x86, 0x00, 0x00, dado.getBytes(), 0);
-
-var dado = new ASN1(dadobin);
-assert(dado.tag == 0x7C);
-assert(dado.elements == 1);
-var ephemeralPublicKeyICC = dado.get(0);
-assert(ephemeralPublicKeyICC.tag == 0x84);
-
-pace.performKeyAgreement(ephemeralPublicKeyICC.value);
+eac.performTerminalAuthentication(termkey, ad.getBytes());
 
 
-var authToken = pace.calculateAuthenticationToken();
 
-var dado = new ASN1(0x7C, new ASN1(0x85, authToken));
+/*
+// card.setCredential(sm);
+mf.setCredential(CardFile.ALL, Card.ALL, sm);
 
-dadobin = card.sendApdu(0x00, 0x86, 0x00, 0x00, dado.getBytes(), 0);
+var ef = new CardFile(mf, ":011C");
+ef.readBinary();
 
-var dado = new ASN1(dadobin);
-print(dado);
-assert(dado.tag == 0x7C);
-assert(dado.elements == 1);
-var authTokenDO = dado.get(0);
-assert(authTokenDO.tag == 0x86);
+mf.sendSecMsgApdu(Card.CPRO|Card.RPRO|Card.RENC, 0x00, 0xB0, 0, 0, 65536);
 
-if (pace.verifyAuthenticationToken(authTokenDO.value)) {
-	print("Authentication token valid");
-}
-
-print(card);
-print(pace);
+*/
