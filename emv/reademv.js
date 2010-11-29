c1ac1068214c7e85febf00eb274136e94cff591b
Andreas Schwier <andreas.schwier@cardcontact.de>
2010-11-29 20:33:34 +0000

    Fixed some documentation issues

--- emv/reademv.js
+++ emv/reademv.js
@@ -1,13 +1,28 @@
-//
-//  ---------
-// |.##> <##.|  CardContact Software & System Consulting
-// |#       #|  32429 Minden, Germany (www.cardcontact.de)
-// |#       #|  Copyright (c) 1999-2005. All rights reserved
-// |'##> <##'|  See file COPYING for details on licensing
-//  --------- 
-//
-// Read some data from MasterCard EMV card
-// 
+/**
+ *  ---------
+ * |.##> <##.|  Open Smart Card Development Platform (www.openscdp.org)
+ * |#       #|  
+ * |#       #|  Copyright (c) 1999-2009 CardContact Software & System Consulting
+ * |'##> <##'|  Andreas Schwier, 32429 Minden, Germany (www.cardcontact.de)
+ *  --------- 
+ *
+ *  This file is part of OpenSCDP.
+ *
+ *  OpenSCDP is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License version 2 as
+ *  published by the Free Software Foundation.
+ *
+ *  OpenSCDP is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with OpenSCDP; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * @fileoverview Simple script to read all data elements from a card card application
+ */
 
 try	{
 	var card = new Card(_scsh3.reader);
