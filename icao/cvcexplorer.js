6c91357c65e789f3d0fb24ac485767690c4fc298
Andreas Schwier <andreas.schwier@cardcontact.de>
2024-05-17 12:37:42 +0200

    Fix CVC for which rights can not be listed

--- icao/cvcexplorer.js
+++ icao/cvcexplorer.js
@@ -1,10 +1,10 @@
 /**
  *  ---------
  * |.##> <##.|  Open Smart Card Development Platform (www.openscdp.org)
- * |#       #|  
+ * |#       #|
  * |#       #|  Copyright (c) 1999-2009 CardContact Software & System Consulting
  * |'##> <##'|  Andreas Schwier, 32429 Minden, Germany (www.cardcontact.de)
- *  --------- 
+ *  ---------
  *
  *  This file is part of OpenSCDP.
  *
@@ -20,7 +20,7 @@
  *  You should have received a copy of the GNU General Public License
  *  along with OpenSCDP; if not, write to the Free Software
  *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
- * 
+ *
  * @fileoverview Tool to decode and dump CVC certificates
  */
 
@@ -59,19 +59,19 @@ CVCExplorer.REMOVESTR = "Remove";
 CVCExplorer.loadBinaryFile = function(filename) {
 	// Open stream
 	var f = new java.io.FileInputStream(filename);
-	
+
 	// Determine file size
 	var flen = f.available();
 
 	// Allocate native byte array
 	var bs = java.lang.reflect.Array.newInstance(java.lang.Byte.TYPE, flen);
-	
+
 	// Read into byte array
 	var len = f.read(bs);
 
 	// Allocate JavaScript ByteBuffer from native/wrapped byte array
 	var bb = new ByteBuffer(bs);
-	
+
 	// Convert to JavaScript ByteString
 	var data = bb.toByteString();
 
@@ -89,8 +89,10 @@ CVCExplorer.dumpCertificate = function(cvc) {
 	print("-----8<----------8<----------8<----------8<----------8<----------8<----------8<----------8<-----");
 	print(cvc);
 	var list = cvc.getRightsAsList();
-	for (var i = 0; i < list.length; i++) {
-		print("  " + list[i]);
+	if (list != null) {
+		for (var i = 0; i < list.length; i++) {
+			print("  " + list[i]);
+		}
 	}
 	print(cvc.getASN1());
 }
@@ -107,25 +109,25 @@ CVCExplorer.prototype.selectFile = function() {
 	if (!filename) {
 		filename = "";
 	}
-	
+
 	var select = Dialog.prompt("Select CVC file", filename, null, "*.cvcert");
-	
+
 	if (select != null) {
 		_scsh3.setProperty("lastcvc", select.replace(/\\/g, "/"));
-		
+
 		var bin = CVCExplorer.loadBinaryFile(select);
-		
+
 		var cvc = new CVC(bin);
 		cvc.decorate();
-		
+
 		var fn = new OutlineNode(select);
 		fn.cvc = cvc;
 		fn.setContextMenu([CVCExplorer.DUMPSTR, CVCExplorer.REMOVESTR]);
 		fn.setUserObject(this);
-		
+
 		this.node.insert(fn);
 		fn.insert(cvc.getASN1());
-		
+
 		CVCExplorer.dumpCertificate(cvc);
 	}
 }
@@ -137,7 +139,7 @@ CVCExplorer.prototype.selectFile = function() {
  *
  * @param {OutlineNode} source the object to which the context menu is associated
  * @param {String} action the action selected from the context menu
- */  
+ */
 CVCExplorer.prototype.actionListener = function(source, action) {
 
 	switch(action) {
